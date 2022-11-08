bc696885829ee3d43bcf3680e368be130673ee2a
matydiaz434233 <102767133+matydiaz434233@users.noreply.github.com>
2022-11-08 15:21:54 -0300

    armo carrosuel1

--- carrousel.js
+++ carrousel.js
@@ -32,3 +32,41 @@ prev.addEventListener('click', () => {
 
 
 mount()
+
+
+// carrousel   dos
+
+const slider_content1 = document.querySelectorAll('.slider_content1')
+const siguiente = document.querySelector('.siguiente')
+const previo = document.querySelector('.previo')
+let indice = 0
+
+
+const mount1 = () => slider_content1[indice].classList.add('active')
+const unmount1 = () => slider_content1[indice].classList.remove('active')
+siguiente.addEventListener('click', () => {
+    unmount1()
+    if(indice >= slider_content1.length - 1){
+        indice = 0
+    }
+    else {
+        indice++
+    }
+    mount1()
+})
+
+previo.addEventListener('click', () => {
+    unmount1()
+    if(indice <= 0){
+        indice = slider_content1.length - 1
+    }
+    else {
+        indice--
+    }
+    mount1()
+})
+
+
+
+
+mount1()
