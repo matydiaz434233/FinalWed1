b9a04b86b4ce8af0634468fbc7ab1a1d9c095a9f
matydiaz434233 <102767133+matydiaz434233@users.noreply.github.com>
2023-05-08 15:54:35 -0300

    asd

--- carrousel.js
+++ carrousel.js
@@ -1,74 +1,63 @@
-const slider_content = document.querySelectorAll('.slider_content')
-const next = document.querySelector('.next')
-const prev = document.querySelector('.prev')
-let index = 0
-alert("si nesecitas adermicina preciona aceptar")
-alert("RIVER 1 - boca 0")
+
+alert("RIVER 1 - boca 0      =>   07/05/2023");
+alert("si nesecitas adermicina preciona aceptar");
+const slider_content = document.querySelectorAll(".slider_content");
+const next = document.querySelector(".next");
+const prev = document.querySelector(".prev");
+let index = 0;
 
 
-const mount = () => slider_content[index].classList.add('active')
-const unmount = () => slider_content[index].classList.remove('active')
-next.addEventListener('click', () => {
-    unmount()
-    if(index >= slider_content.length - 7){
-        index = 0
-    }
-    else {
-        index++
-    }
-    mount()
-})
+const mount = () => slider_content[index].classList.add("active");
+const unmount = () => slider_content[index].classList.remove("active");
+next.addEventListener("click", () => {
+  unmount();
+  if (index >= slider_content.length - 7) {
+    index = 0;
+  } else {
+    index++;
+  }
+  mount();
+});
 
-prev.addEventListener('click', () => {
-    unmount()
-    if(index <= 0){
-        index = slider_content.length - 7
-    }
-    else {
-        index--
-    }
-    mount()
-})
+prev.addEventListener("click", () => {
+  unmount();
+  if (index <= 0) {
+    index = slider_content.length - 7;
+  } else {
+    index--;
+  }
+  mount();
+});
 
-
-
-
-mount()
-
+mount();
 
 // carrousel   dos
 
-const slider_content1 = document.querySelectorAll('.slider_content1')
-const siguiente = document.querySelector('.siguiente')
-const previo = document.querySelector('.previo')
-let indice = 0
+const slider_content1 = document.querySelectorAll(".slider_content1");
+const siguiente = document.querySelector(".siguiente");
+const previo = document.querySelector(".previo");
+let indice = 0;
 
+const mount1 = () => slider_content1[indice].classList.add("active");
+const unmount1 = () => slider_content1[indice].classList.remove("active");
+siguiente.addEventListener("click", () => {
+  unmount1();
+  if (indice >= slider_content1.length - 1) {
+    indice = 0;
+  } else {
+    indice++;
+  }
+  mount1();
+});
 
-const mount1 = () => slider_content1[indice].classList.add('active')
-const unmount1 = () => slider_content1[indice].classList.remove('active')
-siguiente.addEventListener('click', () => {
-    unmount1()
-    if(indice >= slider_content1.length - 1){
-        indice = 0
-    }
-    else {
-        indice++
-    }
-    mount1()
-})
+previo.addEventListener("click", () => {
+  unmount1();
+  if (indice <= 0) {
+    indice = slider_content1.length - 1;
+  } else {
+    indice--;
+  }
+  mount1();
+});
 
-previo.addEventListener('click', () => {
-    unmount1()
-    if(indice <= 0){
-        indice = slider_content1.length - 1
-    }
-    else {
-        indice--
-    }
-    mount1()
-})
-
-
-
-
-mount1()
+mount1();
